9387bbf96f4e3c02301ef448016b3519e6f060e4
190030406_DHIREN DOMMETI <190030406@kluniversity.in>
2021-07-30 14:37:05 +0530

    Updated routes

---
+++ src/main/java/net/guides/springboot2/crud/controller/LoadController.java
@@ -0,0 +1,77 @@
+package net.guides.springboot2.crud.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.guides.springboot2.crud.exception.ResourceNotFoundException;
+import net.guides.springboot2.crud.model.Load;
+import net.guides.springboot2.crud.repository.LoadRepository;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class LoadController {
+    @Autowired
+    private LoadRepository LoadRepository;
+
+    @GetMapping("/load")
+    public List<Load> getAllEmployees() {
+        return LoadRepository.findAll();
+    }
+
+    @GetMapping("/load/{id}")
+    public ResponseEntity<Load> getShipperId(@PathVariable(value = "id") Long shipperId)
+            throws ResourceNotFoundException {
+        Load load = LoadRepository.findById(shipperId)
+                .orElseThrow(() -> new ResourceNotFoundException("Load not found for this shipper Id :: " + shipperId));
+        return ResponseEntity.ok().body(load);
+    }
+
+    @PostMapping("/load")
+    public Load createLoad(@Valid @RequestBody Load load) {
+        return LoadRepository.save(load);
+    }
+
+    @PutMapping("/load/{id}")
+    public ResponseEntity<Load> updateLoad(@PathVariable(value = "id") Long shipperId,
+                                               @Valid @RequestBody Load loadDetails) throws ResourceNotFoundException {
+        Load load = LoadRepository.findById(shipperId)
+                .orElseThrow(() -> new ResourceNotFoundException("Load not found for this shipper Id  :: " + shipperId));
+
+        load.setProductType(loadDetails.getProductType());
+        load.setUnloadingPoint(loadDetails.getUnloadingPoint());
+        load.setLoadingPoint(loadDetails.getLoadingPoint());
+        load.setTruckType(loadDetails.getTruckType());
+        load.setNoOfTrucks(loadDetails.getNoOfTrucks());
+        load.setWeight(loadDetails.getWeight());
+
+        final Load updatedLoad = LoadRepository.save(load);
+        return ResponseEntity.ok(updatedLoad);
+    }
+
+    @DeleteMapping("/load/{id}")
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long shipperId)
+            throws ResourceNotFoundException {
+        Load load = LoadRepository.findById(shipperId)
+                .orElseThrow(() -> new ResourceNotFoundException("Load not found for this shipper Id :: " + shipperId));
+
+        LoadRepository.delete(load);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+}
---
+++ src/main/java/net/guides/springboot2/crud/model/Load.java
@@ -0,0 +1,105 @@
+package net.guides.springboot2.crud.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "load")
+public class Load {
+
+    private long shipperId;
+    private String loadingPoint;
+    private String unloadingPoint;
+    private String productType;
+    private String truckType;
+    private String noOfTrucks;
+    private String weight;
+    //private String Date;
+
+    public Load() {
+
+    }
+
+
+    public Load(String loadingPoint,
+                String unloadingPoint,
+                String productType,
+                String truckType,
+                String noOfTrucks,
+                String weight) {
+        this.loadingPoint = loadingPoint;
+        this.unloadingPoint = unloadingPoint;
+        this.productType = productType;
+        this.truckType = truckType;
+        this.noOfTrucks = noOfTrucks;
+        this.weight = weight;
+    }
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        public long getShipperId() {
+        return shipperId;
+    }
+    public void setShipperId(long shipperId) {
+        this.shipperId = shipperId;
+    }
+ 
+    @Column(name = "loadingPoint", nullable = false)
+    public String getLoadingPoint() {
+        return loadingPoint;
+    }
+    public void setLoadingPoint(String loadingPoint) {
+        this.loadingPoint = loadingPoint;
+    }
+ 
+    @Column(name = "unloadingPoint", nullable = false)
+    public String getUnloadingPoint() {
+        return unloadingPoint;
+    }
+    public void setUnloadingPoint(String unloadingPoint) {
+        this.unloadingPoint = unloadingPoint;
+    }
+ 
+    @Column(name = "productType", nullable = false)
+    public String getProductType() {
+        return productType;
+    }
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    @Column(name = "truckType", nullable = false)
+    public String getTruckType() {
+        return truckType;
+    }
+    public void setTruckType(String truckType) {
+        this.truckType = truckType;
+    }
+
+    @Column(name = "noOfTrucks", nullable = false)
+    public String getNoOfTrucks() {
+        return noOfTrucks;
+    }
+    public void setNoOfTrucks(String noOfTrucks) {
+        this.noOfTrucks = noOfTrucks;
+    }
+
+    @Column(name = "weight", nullable = false)
+    public String getWeight() {
+        return weight;
+    }
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [shipperId=" + shipperId + ", loadingPoint=" + loadingPoint + ", unloadingPoint=" + unloadingPoint + ", productType=" + productType
+       + ", truckType=" + truckType + ", noOfTrucks=" + noOfTrucks + ", weight=" + weight + "]";
+    }
+ 
+}
